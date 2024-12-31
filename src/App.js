e3bd09dff4f712b040f377766e9c239c69361c16
synnefon <connorjhopkins@gmail.com>
2024-12-31 06:39:07 -0800

    better deets

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="title">
-          Welcome to my personal site
+    <div className="app-base">
+      <body className="App-body">
+        <div className="content-wrapper">
+          <h2 className="title">Connor Hopkins</h2>
+          <h6 className="description">Software Engineer, Thing-Maker, Dungeon Master, Fictional Character.</h6>
+          <div className="links">
+            <p><a className="link" href="https://www.linkedin.com/in/connor-j-hopkins" rel="noreferrer">linkedin</a></p>
+            <p><a className="link" href="https://github.com/synnefon" rel="noreferrer">github</a></p>
+            <p><a className="link" href="https://thangs.com/designer/synnefon" rel="noreferrer">3D models</a></p>
+          </div>
         </div>
-        <div className="links">
-          <a className="link" href="https://www.linkedin.com/in/connor-j-hopkins" rel="noreferrer">
-            linkedin
-          </a>
-          <br/>
-          <a className="link" href="https://github.com/synnefon" rel="noreferrer">
-            github
-          </a>
-        </div>
-      </header>
+      </body>
     </div>
   );
 }
