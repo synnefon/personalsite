bcf633f5662500787b9e8a676d8ae38cef256787
synnefon <connorjhopkins@gmail.com>
2024-12-31 04:50:25 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="title">
+          Welcome to my personal site
+        </div>
+        <div className="links">
+          <a className="link" href="https://www.linkedin.com/in/connor-j-hopkins" rel="noreferrer">
+            linkedin
+          </a>
+          <br/>
+          <a className="link" href="https://github.com/synnefon" rel="noreferrer">
+            github
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
