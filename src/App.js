fca6596073b688f9f2e7ede0b556c1991067a4ca
synnefon <connorjhopkins@gmail.com>
2024-12-31 15:32:44 -0800

    adding color

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ function App() {
           <h2 className="title">Connor Hopkins</h2>
           <h6 className="description">Software Engineer, Thing-Maker, Dungeon Master, Fictional Character.</h6>
           <div className="links">
-            <p><a className="link" href="https://www.linkedin.com/in/connor-j-hopkins" rel="noreferrer">linkedin</a></p>
-            <p><a className="link" href="https://github.com/synnefon" rel="noreferrer">github</a></p>
-            <p><a className="link" href="https://thangs.com/designer/synnefon" rel="noreferrer">3D models</a></p>
+              <a className="link" href="https://github.com/synnefon" rel="noreferrer">
+                <p className="link-text">github</p>
+              </a>
+              <a className="link" href="https://www.linkedin.com/in/connor-j-hopkins" rel="noreferrer">
+                <p className="link-text">linkedin</p>
+              </a>
+              <a className="link" href="https://thangs.com/designer/synnefon" rel="noreferrer">
+                <p className="link-text">3D models</p>
+              </a>
           </div>
         </div>
       </body>
