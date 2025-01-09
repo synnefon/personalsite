e01ec2fe463136d4b8954ef3d7e0dc3bb4316dd3
synnefon <connorjhopkins@gmail.com>
2025-01-08 20:43:33 -0800

    using hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Home from './home/Home';
 import Projects from './projects/Projects';
@@ -6,13 +6,13 @@ import Wip from './projects/Wip';
 
 function App() {
   return (
-    <BrowserRouter> 
+    <Router> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/wip" element={<Wip />} />
       </Routes>
-    </BrowserRouter> 
+    </Router> 
   );
 }
 
