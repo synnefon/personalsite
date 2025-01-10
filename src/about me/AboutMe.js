3346498a17a94d0d75635c492f771e16b51b2b4c
synnefon <connorjhopkins@gmail.com>
2025-01-10 05:24:47 -0800

    colorizing

--- src/about me/AboutMe.js
+++ src/about me/AboutMe.js
@@ -1,16 +1,11 @@
 import Self from "./Self";
 
+import '../styles/aboutme.css'
+
 export default function AboutMe() {
   return (
-    <div
-      id="app-base"
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column'
-      }}>
-        <h1>page under construction</h1>
+    <div id='app-base' className="about-colors">
+        <h1 className="about-me-title about-colors">page under construction</h1>
         <Self/>
     </div>
   );
--- src/home/Home.js
+++ src/home/Home.js
@@ -18,44 +18,44 @@ export default function Home() {
   }
 
   return (
-    <div id="app-base" className='invert-color'>
-      <div className="content-wrapper invert-color">
+    <div id="app-base" className='home-colors'>
+      <div className="content-wrapper home-colors">
         <h2 className="title">connor hopkins</h2>
-        <h5 className="description invert-color">
-          <span className="bracket invert-color">{'{ '}</span>
+        <h5 className="description home-colors">
+          <span className="bracket home-colors">{'{ '}</span>
           <TypeAnimation
-            className='description-text invert-color'
+            className='description-text home-colors'
             sequence={descriptors.flatMap((d) => extractDescription(d))}
             wrapper="span"
             deletionSpeed={60}
             repeat={Infinity}
           />
-          <span className="bracket invert-color">{'}'}</span>
+          <span className="bracket home-colors">{'}'}</span>
         </h5>
-        <div className="links invert-color">
-          <Link className="link top left invert-color" to="/about" rel="noreferrer">
-            <p className="link-text invert-color">about me</p>
-            <p className="tooltip-text invert-color">$ whois connorhopkins.dev</p>
+        <div className="links home-colors">
+          <Link className="link top left home-colors" to="/about" rel="noreferrer">
+            <p className="link-text home-colors">about me</p>
+            <p className="tooltip-text home-colors">$ whois connorhopkins.dev</p>
           </Link>
-          <Link className="link top right invert-color" to="/projects" rel="noreferrer">
-            <p className="link-text invert-color">projects</p>
-            <p className="tooltip-text invert-color">an assortment of web-accessible work</p>
+          <Link className="link top right home-colors" to="/projects" rel="noreferrer">
+            <p className="link-text home-colors">projects</p>
+            <p className="tooltip-text home-colors">an assortment of web-accessible work</p>
           </Link>
-          <Link className="link middle left invert-color" to="/wip" rel="noreferrer">
-            <p className="link-text invert-color">resume</p>
-            <p className="tooltip-text invert-color">the list of stuff i've done professionally</p>
+          <Link className="link middle left home-colors" to="/wip" rel="noreferrer">
+            <p className="link-text home-colors">resume</p>
+            <p className="tooltip-text home-colors">the list of stuff i've done professionally</p>
           </Link>
-          <a className="link middle right invert-color" href="https://github.com/synnefon" rel="noreferrer">
-            <p className="link-text invert-color">github</p>
-            <p className="tooltip-text invert-color">where you can see some code i've written</p>
+          <a className="link middle right home-colors" href="https://github.com/synnefon" rel="noreferrer">
+            <p className="link-text home-colors">github</p>
+            <p className="tooltip-text home-colors">where you can see some code i've written</p>
           </a>
-          <a className="link bottom left invert-color" href="https://www.linkedin.com/in/connor-j-hopkins" rel="noreferrer">
-            <p className="link-text invert-color">linkedin</p>
-            <p className="tooltip-text invert-color">let's network!</p>
+          <a className="link bottom left home-colors" href="https://www.linkedin.com/in/connor-j-hopkins" rel="noreferrer">
+            <p className="link-text home-colors">linkedin</p>
+            <p className="tooltip-text home-colors">let's network!</p>
           </a>
-          <Link className="link bottom right invert-color" to="/wip" rel="noreferrer">
-            <p className="link-text invert-color">get in touch</p>
-            <p className="tooltip-text invert-color">an email form</p>
+          <Link className="link bottom right home-colors" to="/wip" rel="noreferrer">
+            <p className="link-text home-colors">get in touch</p>
+            <p className="tooltip-text home-colors">an email form</p>
           </Link>
         </div>
       </div>
