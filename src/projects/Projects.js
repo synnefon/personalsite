2857d5159689fc0068357bb5cd4b2a09187775a0
synnefon <connorjhopkins@gmail.com>
2025-10-09 01:07:09 -0700

    shuffling projects

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,28 +1,30 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import Raincloud from '../raincloud/Raincloud';
+// import Raincloud from '../raincloud/Raincloud';
 
 import '../styles/projects.css';
 import '../styles/app.css';
 
 export default function Projects() {
-  const [showLightning, setShowLightning] = useState(false);
+  // const [showLightning, setShowLightning] = useState(false);
 
-  useEffect(() => {
-    document.getElementById("footer").style.backgroundColor = `${showLightning ? 'transparent' : ''}`;
-  }, [showLightning])
+  // useEffect(() => {
+  //   document.getElementById("footer").style.backgroundColor = `${showLightning ? 'transparent' : ''}`;
+  // }, [showLightning])
 
   return (
-    <div id="app-base" className={`proj-colors ${showLightning ? 'lightning' : ''}`}>
+    // <div id="app-base" className={`proj-colors ${showLightning ? 'lightning' : ''}`}>
+    <div id="app-base" className="proj-colors">
       <div className="content-wrapper proj-colors">
         <h2 className="title proj-colors">projects</h2>
         <h5 className="description proj-colors"> an assortment of web-accessible work </h5>
         <div className="links proj-colors">
-          <a className="link proj-colors top left" href="https://thangs.com/designer/synnefon" rel="noreferrer">
-            <p className="link-text proj-colors">3d models</p>
-            <p className="tooltip-text proj-colors">a collection of my 3d-printable work</p>
+          <a className="link proj-colors top left" href="https://synnefon.github.io/mapinator" rel="noreferrer">
+            <p className="link-text proj-colors">mapinator</p>
+            <p className="tooltip-text proj-colors">generates terrain maps</p>
           </a>
+
           <Link className="link proj-colors top right" to="/sudoku" rel="noreferrer">
             <p className="link-text proj-colors">sudoku</p>
             <p className="tooltip-text proj-colors">an eternal classic</p>
@@ -31,15 +33,16 @@ export default function Projects() {
             <p className="link-text proj-colors">sea match</p>
             <p className="tooltip-text proj-colors">a sea-themed memory game</p>
           </Link> */}
-          <a className="link proj-colors middle left" href="https://synnefon.github.io/mapinator" rel="noreferrer">
-            <p className="link-text proj-colors">mapinator</p>
-            <p className="tooltip-text proj-colors">generates terrain maps</p>
+          <a className="link proj-colors middle left" href="https://thangs.com/designer/synnefon" rel="noreferrer">
+            <p className="link-text proj-colors">3d models</p>
+            <p className="tooltip-text proj-colors">a collection of my 3d-printable work</p>
           </a>
+
           {
-              <Link className="link proj-colors middle right" to="/snek" rel="noreferrer">
-                <p className="link-text proj-colors">snek</p>
-                <p className="tooltip-text proj-colors">snek!</p>
-              </Link>
+            <Link className="link proj-colors middle right" to="/snek" rel="noreferrer">
+              <p className="link-text proj-colors">snek</p>
+              <p className="tooltip-text proj-colors">snek!</p>
+            </Link>
           }
           <a className="link proj-colors middle left" href="https://www.npmjs.com/package/img-butler" rel="noreferrer">
             <p className="link-text proj-colors">img-butler</p>
@@ -53,17 +56,13 @@ export default function Projects() {
             <p className="link-text proj-colors">work in progress</p>
             <p className="tooltip-text proj-colors">coming soon to a website near you</p>
           </Link> */}
-          {/* <a className="link proj-colors bottom left" href="http://18.190.107.78" rel="noreferrer">
-            <p className="link-text proj-colors">shufflenator</p>
-            <p className="tooltip-text proj-colors">gives a card deck's optimal shuffle pattern</p>
-          </a> */}
-          {/* <Link className="link proj-colors bottom left" to="/shufflenator" rel="noreferrer">
-            <p className="link-text proj-colors">shufflenator</p>
-            <p className="tooltip-text proj-colors">gives a card deck's optimal shuffle pattern</p>
-          </Link> */}
-          <Link className="link proj-colors bottom left" to="/toolbox" rel="noreferrer">
+          {/* <Link className="link proj-colors bottom left" to="/toolbox" rel="noreferrer">
             <p className="link-text proj-colors">toolbox</p>
             <p className="tooltip-text proj-colors">various utilities</p>
+          </Link> */}
+          <Link className="link proj-colors bottom left" to="/shufflenator" rel="noreferrer">
+            <p className="link-text proj-colors">shufflenator</p>
+            <p className="tooltip-text proj-colors">gives a card deck's optimal shuffle pattern</p>
           </Link>
           <Link className="link proj-colors bottom right" to="/game-of-life" rel="noreferrer">
             <p className="link-text proj-colors">game of life</p>
