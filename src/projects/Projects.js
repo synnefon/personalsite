2fae74a6beb0192531d159b406e8df2de65b5b50
connor <connor@yoodli.ai>
2025-10-31 02:22:49 -0700

    facelift

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -18,16 +18,16 @@ export default function Projects() {
     <div id="app-base" className="proj-colors">
       <div className="content-wrapper proj-colors">
         <h2 className="title proj-colors">projects</h2>
-        <h5 className="description proj-colors"> an assortment of web-accessible work </h5>
+        <h5 className="description proj-colors">assortment of web-accessible work </h5>
         <div className="links proj-colors">
           <a className="link proj-colors top left" href="https://synnefon.github.io/mapinator" rel="noreferrer">
             <p className="link-text proj-colors">mapinator</p>
-            <p className="tooltip-text proj-colors">procedurally generates terrain maps</p>
+            <p className="tooltip-text proj-colors">procedural terrain map generator</p>
           </a>
 
           <Link className="link proj-colors top right" to="/sudoku" rel="noreferrer">
             <p className="link-text proj-colors">sudoku</p>
-            <p className="tooltip-text proj-colors">an eternal classic</p>
+            <p className="tooltip-text proj-colors">eternal classic</p>
           </Link>
           {/* <Link className="link proj-colors middle left" to="/matchgame" rel="noreferrer">
             <p className="link-text proj-colors">sea match</p>
@@ -35,7 +35,7 @@ export default function Projects() {
           </Link> */}
           <a className="link proj-colors middle left" href="https://thangs.com/designer/synnefon" rel="noreferrer">
             <p className="link-text proj-colors">3d models</p>
-            <p className="tooltip-text proj-colors">a collection of my 3d-printable work</p>
+            <p className="tooltip-text proj-colors">collection of my 3d-printable work</p>
           </a>
 
           {
@@ -46,11 +46,11 @@ export default function Projects() {
           }
           <a className="link proj-colors middle left" href="https://www.npmjs.com/package/img-butler" rel="noreferrer">
             <p className="link-text proj-colors">img-butler</p>
-            <p className="tooltip-text proj-colors">a react-based image interaction library</p>
+            <p className="tooltip-text proj-colors">image interaction npm package</p>
           </a>
           <a className="link proj-colors middle right" href="https://github.com/synnefon/spagett" rel="noreferrer">
             <p className="link-text proj-colors">spagett ql</p>
-            <p className="tooltip-text proj-colors">lisp-like query language. al dente, of course. </p>
+            <p className="tooltip-text proj-colors">lisp-like query language. al dente. </p>
           </a>
           {/* <Link className="link proj-colors middle right" to="/wip" rel="noreferrer">
             <p className="link-text proj-colors">work in progress</p>
@@ -62,11 +62,11 @@ export default function Projects() {
           </Link> */}
           <Link className="link proj-colors bottom left" to="/shufflenator" rel="noreferrer">
             <p className="link-text proj-colors">shufflenator</p>
-            <p className="tooltip-text proj-colors">gives a card deck's optimal shuffle pattern</p>
+            <p className="tooltip-text proj-colors">optimal shuffle pattern calculator</p>
           </Link>
           <Link className="link proj-colors bottom right" to="/game-of-life" rel="noreferrer">
             <p className="link-text proj-colors">game of life</p>
-            <p className="tooltip-text proj-colors">conway's game of life</p>
+            <p className="tooltip-text proj-colors">cellular automata simulator</p>
           </Link>
           {/* <Link className="link proj-colors bottom right" to="/wip" rel="noreferrer">
             <p className="link-text proj-colors">rpg tabletop</p>
