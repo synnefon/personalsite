ae89600059b3bfbedc9a96a66e597c8fa94b378c
connor <connor@yoodli.ai>
2025-12-29 00:26:39 -0800

    stylish shavian

--- src/shavianator/Shavianator.js
+++ src/shavianator/Shavianator.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { shavianateSentence } from "./shavianating";
 
+import "../styles/shavianator.css";
+
 export default function Shavianator() {
   console.log(shavianateSentence("Hello world"));
   const [sentence, setSentence] = useState("");
@@ -8,16 +10,17 @@ export default function Shavianator() {
     setSentence(e.target.value);
   };
   return (
-    <div id="app-base">
-      <div className="content-wrapper">
-        <h2 className="title">shavianator</h2>
+    <div id="app-base" className="shavianator-colors">
+      <div className="content-wrapper shavianator-colors">
+        <h2 className="title shavianator-colors">shavianator</h2>
         <input
           type="text"
+          className="shavianator-input"
           placeholder="Enter a sentence to shavianate"
           value={sentence}
           onChange={handleSentenceChange}
         />
-        <p>{shavianateSentence(sentence)}</p>
+        <p className="shavianator-output">{shavianateSentence(sentence)}</p>
       </div>
     </div>
   );
