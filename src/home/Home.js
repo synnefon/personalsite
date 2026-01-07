5ef84476da3712df977831f54ad076a8484eed5e
connor <connor@yoodli.ai>
2026-01-06 22:39:30 -0800

    refactor Home component by removing unused color interpolation functions and imports

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,29 +1,9 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
 
 import "../styles/app.css";
 import "../styles/home.css";
-
-const hexToRgb = (hex) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
-};
-
-const interpolateColor = (color1, color2, factor) => {
-  const c1 = hexToRgb(color1);
-  const c2 = hexToRgb(color2);
-  const r = Math.round(c1.r + factor * (c2.r - c1.r));
-  const g = Math.round(c1.g + factor * (c2.g - c1.g));
-  const b = Math.round(c1.b + factor * (c2.b - c1.b));
-  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
-};
-
-const COLORS = ["#201658", "#1D24CA"];
 
 export default function Home() {
   // const [color, setColor] = useState(COLORS[0]);
